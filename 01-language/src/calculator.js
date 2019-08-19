e066c84f138025199592e13847ef88b29500b6d9
Magesh Kuppan <tkmagesh77@gmail.com>
2019-08-19 14:50:02 +0530

    refactoring calculator - final

--- 01-language/src/calculator.js
+++ 01-language/src/calculator.js
@@ -1,17 +1,18 @@
 function add(x,y){
 	function parseArg(n){
-		if (Array.isArray(n)){
-			var result = 0;
-			for(var index = 0, count = n.length; index < count; index++)
-				result += parseArg(n[index]);
-			return result;
-		}
+		if (Array.isArray(n)) return add.apply(undefined ,n);
 		if (typeof n === 'function') return parseArg(n());
 		return isNaN(n) ? 0 : parseInt(n,10);
-			
 	}
-	var result = 0;
-	for(var index = 0, count = arguments.length; index < count; index++)
-		result += parseArg(arguments[index]);
-	return result;
+	return arguments.length <= 1 ? parseArg(arguments[0]) : parseArg(arguments[0]) + add([].slice.call(arguments,1));
 }
+
+/*
+As a method of an object
+	this -> object
+
+As a function
+	this -> global (window)
+
+
+*/
